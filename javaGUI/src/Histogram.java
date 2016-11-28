95769985b41bca8b7a7986408b7410d1e1a18f94
Juarez <juarez.asf@gmail.com>
2016-11-28 01:48:34 -0200

    improve demo code

--- javaGUI/src/Histogram.java
+++ javaGUI/src/Histogram.java
@@ -33,31 +33,7 @@ public class Histogram {
 
     public Histogram() {
         panel = new JPanel(new BorderLayout());
-        String[] seriesNames = new String[]{"2001", "2002"};
-        String[] categoryNames = new String[]{"First Quater",
-                "Second Quater"};
-        Number[][] categoryData = new Integer[][]{{new Integer(20),
-                new Integer(35)},
-                {new Integer(40),
-                        new Integer(60)}
-        };
-        CategoryDataset categoryDataset = new DefaultCategoryDataset(seriesNames,
-                categoryNames,
-                categoryData);
 
-        JFreeChart chart = ChartFactory.createVerticalBarChart3D
-                ("Sample Category Chart", // Title
-                        "Quarters", // X-Axis label
-                        "Sales", // Y-Axis label
-                        categoryDataset, // Dataset
-                        true // Show legend
-                );
-
-        BufferedImage image = chart.createBufferedImage(500, 300);
-
-        imageLabel.setIcon(new ImageIcon(image));
-
-        selectedOption.setSelectedIndex(-1);
         selectedOption.addItem("Operation 1");
         selectedOption.addItem("Operation 2");
 
@@ -66,8 +42,53 @@ public class Histogram {
             @Override
             public void actionPerformed(ActionEvent e) {
 
+                int selected = selectedOption.getSelectedIndex();
+
+                String[] seriesNames = new String[]{"2001", "2002"};
+                String[] categoryNames = new String[]{"First Quater",
+                        "Second Quater"};
+                Number[][] categoryData = new Integer[][]{{new Integer(20),
+                        new Integer(35)},
+                        {new Integer(40),
+                                new Integer(60)}
+                };
+                switch (selected) {
+                    case 0:
+                        categoryData = new Integer[][]{{new Integer(20),
+                                new Integer(35)},
+                                {new Integer(40),
+                                        new Integer(60)}
+                        };
+
+                        break;
+                    case 1:
+                        categoryData = new Integer[][]{{new Integer(35),
+                                new Integer(20)},
+                                {new Integer(60),
+                                        new Integer(40)}
+                        };
+                        break;
+                    default:
+                        break;
+                }
+                CategoryDataset categoryDataset = new DefaultCategoryDataset(seriesNames,
+                        categoryNames,
+                        categoryData);
+
+                JFreeChart chart = ChartFactory.createVerticalBarChart3D
+                        ("Sample Category Chart", // Title
+                                "Quarters", // X-Axis label
+                                "Sales", // Y-Axis label
+                                categoryDataset, // Dataset
+                                true // Show legend
+                        );
+                BufferedImage image = chart.createBufferedImage(500, 300);
+
+                imageLabel.setIcon(new ImageIcon(image));
+
             }
         });
+        selectedOption.setSelectedIndex(0);
 
         panel.add(selectedOption, BorderLayout.NORTH);
         panel.add(imageLabel, BorderLayout.CENTER);
