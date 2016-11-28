4c2550f001b9f11f17b3906293c9692fe6113b9a
Juarez <juarez.asf@gmail.com>
2016-11-27 23:13:44 -0200

    Added code to learn mappings

---
+++ pyParsers/learnTranslationTable.py
@@ -0,0 +1,67 @@
+import sys
+
+
+def learn(filename, cod_name_pairs):
+    line_counter = 0
+
+    output = [dict() for _ in range(len(cod_name_pairs))]
+    with open(filename, "r") as fp:
+        for line in fp.readlines():
+
+            if line_counter == 0:
+                pass
+            else:
+                field = line.split(";")
+                k = 0
+                for pair in cod_name_pairs:
+                    cod_column_index = pair[0]
+                    name_column_index = pair[1]
+
+                    minimumLength = max(cod_column_index, name_column_index)
+
+                    if len(field) < minimumLength:
+                        print "error! line {} incomplete!".format(line_counter)
+                        continue
+
+                    cod = field[cod_column_index]
+                    name = field[name_column_index]
+
+                    if cod in output[k]:
+                        if name != output[k][cod]:
+                            print "redinition of code:name !"
+                        else:
+                            pass
+                    else:
+                        output[k][cod] = name
+                    k += 1
+
+            if line_counter % 10000 == 0:
+                print ".",
+            if line_counter % 800000 == 0:
+                print "."
+            line_counter += 1
+
+    return output
+
+
+import json
+
+
+def save_to(data, file):
+    print 'saving data to:', file
+    with open(file, "w") as fp:
+        json.dump(data, fp, indent=2, sort_keys=True)
+
+
+if __name__ == "__main__":
+
+    if len(sys.argv) != 2:
+        print "error! inform file to read from!"
+    filename = sys.argv[1]
+
+    mappings = learn(filename, [(2, 3), (4, 5)])
+    municipio_mapping = mappings[0]
+    uf_mapping = mappings[1]
+
+    save_to(municipio_mapping, "municipioMapping,json")
+    save_to(uf_mapping, "ufMapping,json")
